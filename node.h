8b0ea5ed33780cfd8bf41de71bdd5dc45ed0fbb3
Gabriel Gonçalves Smith <gabrielgsmith@outlook.com>
2016-10-31 16:50:40 -0200

    Remove queue and node files: unnecessary

--- node.h
+++
@@ -1,9 +0,0 @@
-typedef struct node
-{
-  int id;
-  struct node* next;
-}node_t;
-
-void node_create (struct node*, int, struct node*);
-int node_get_value (struct node*);
-void free_node (struct node*);
--- queue.h
+++
@@ -1,8 +0,0 @@
-typedef struct queue
-{
-  struct node* first;
-  struct node* last;
-}queue_t;
-
-void queue (queue_t*, int);
-int dequeue (queue_t*);
